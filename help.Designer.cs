7bb9b48881cbab434da7107a6ba554b5b205efb5
Keikuethas <64356744+Keikuethas@users.noreply.github.com>
2021-04-01 16:31:06 +0300

    Add files via upload

---
+++ help.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Жеребьёвка
+{
+    partial class help
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Контактная информация");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Другие проекты");
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Автор", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Основная информация");
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Открыть");
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Категория");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Добавление участника");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Удаление участника");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Редактирование информации об участнике");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Участники", new System.Windows.Forms.TreeNode[] {
+            treeNode7,
+            treeNode8,
+            treeNode9});
+            System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Построение таблицы");
+            System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Заполнение таблицы");
+            System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Ограничение встреч");
+            System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Добавление таблицы к файлу");
+            System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Удаление таблицы для данной категории");
+            System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Открытие таблицы для данной категории");
+            System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Экспорт таблицы в Excel");
+            System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Таблица", new System.Windows.Forms.TreeNode[] {
+            treeNode11,
+            treeNode12,
+            treeNode13,
+            treeNode14,
+            treeNode15,
+            treeNode16,
+            treeNode17});
+            System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Сообщения об успехе");
+            System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Сохранить");
+            System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("Открыть Excel файл");
+            System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Информация о функциях кнопок и прочих элементов", new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode6,
+            treeNode10,
+            treeNode18,
+            treeNode19,
+            treeNode20,
+            treeNode21});
+            System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("Жеребьёвка", new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode22});
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "Узел1";
+            treeNode1.Text = "Контактная информация";
+            treeNode2.Name = "Узел2";
+            treeNode2.Text = "Другие проекты";
+            treeNode3.Name = "Узел0";
+            treeNode3.Text = "Автор";
+            treeNode4.Name = "Узел4";
+            treeNode4.Text = "Основная информация";
+            treeNode5.Name = "Узел6";
+            treeNode5.Text = "Открыть";
+            treeNode6.Name = "Узел7";
+            treeNode6.Text = "Категория";
+            treeNode7.Name = "Узел10";
+            treeNode7.Text = "Добавление участника";
+            treeNode8.Name = "Узел11";
+            treeNode8.Text = "Удаление участника";
+            treeNode9.Name = "Узел12";
+            treeNode9.Text = "Редактирование информации об участнике";
+            treeNode10.Name = "Узел9";
+            treeNode10.Text = "Участники";
+            treeNode11.Name = "Узел15";
+            treeNode11.Text = "Построение таблицы";
+            treeNode12.Name = "Узел16";
+            treeNode12.Text = "Заполнение таблицы";
+            treeNode13.Name = "Узел17";
+            treeNode13.Text = "Ограничение встреч";
+            treeNode14.Name = "Узел21";
+            treeNode14.Text = "Добавление таблицы к файлу";
+            treeNode15.Name = "Узел22";
+            treeNode15.Text = "Удаление таблицы для данной категории";
+            treeNode16.Name = "Узел23";
+            treeNode16.Text = "Открытие таблицы для данной категории";
+            treeNode17.Name = "Узел25";
+            treeNode17.Text = "Экспорт таблицы в Excel";
+            treeNode18.Name = "Узел13";
+            treeNode18.Text = "Таблица";
+            treeNode19.Name = "Узел18";
+            treeNode19.Text = "Сообщения об успехе";
+            treeNode20.Name = "Узел19";
+            treeNode20.Text = "Сохранить";
+            treeNode21.Name = "Узел26";
+            treeNode21.Text = "Открыть Excel файл";
+            treeNode22.Name = "Узел5";
+            treeNode22.Text = "Информация о функциях кнопок и прочих элементов";
+            treeNode23.Name = "Узел3";
+            treeNode23.Text = "Жеребьёвка";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode23});
+            this.treeView1.Size = new System.Drawing.Size(318, 376);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(337, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(451, 376);
+            this.textBox1.TabIndex = 1;
+            // 
+            // help
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 392);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.treeView1);
+            this.Name = "help";
+            this.Text = "Справка";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
